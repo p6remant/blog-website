c35aaac52ffef31973c7ff273e9ce179640adaf4
remant <paudelremant45@gmail.com>
2025-06-07 16:11:58 +0545

    feat: create Login component with reactive form, validation, and styling

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './login/login.component';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, RouterModule, AuthRoutingModule],
+  imports: [CommonModule, RouterModule, AuthRoutingModule, ReactiveFormsModule],
 })
 export class AuthModule {}
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: false,
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+  hidePassword = true;
 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(8)]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+    });
+  }
+
+  togglePasswordVisibility() {
+    this.hidePassword = !this.hidePassword;
+  }
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Login data:', this.loginForm.value);
+      // handle login here
+    }
+  }
+
+  resetForm() {
+    this.loginForm.reset();
+  }
 }
