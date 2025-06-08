661e42d0bc6305dd5e8252fe03f1d48e68c63af1
remant <paudelremant45@gmail.com>
2025-06-08 13:54:35 +0545

    feat: add AuthService with generic GET and POST methods

---
+++ src/app/core/services/auth.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environment/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private http = inject(HttpClient);
+  private baseUrl = environment.apiBaseUrl;
+  private headers = new HttpHeaders({
+    'Content-Type': 'application/json',
+  });
+
+  // Generic GET method, only pass endpoint path
+  get<T>(endpoint: string): Observable<T> {
+    const url = `${this.baseUrl}${endpoint}`;
+    return this.http.get<T>(url, { headers: this.headers });
+  }
+
+  // Generic POST method, only pass endpoint path and payload
+  post<T>(endpoint: string, payload: any): Observable<T> {
+    const url = `${this.baseUrl}${endpoint}`;
+    return this.http.post<T>(url, payload, { headers: this.headers });
+  }
+}
