a0839be8036022998397808a688bcf33ae27de07
remant <paudelremant45@gmail.com>
2025-06-08 13:56:37 +0545

    feat: add auth and post models for login and post management

---
+++ src/app/models/auth.model.ts
@@ -0,0 +1,13 @@
+export interface LoginPayloadTypes {
+  username: string;
+  password: string;
+  expiresInMins?: number;
+}
+
+export interface LoginResponseTypes {
+  username: string;
+  email: string;
+  token: string;
+  accessToken: string;
+  refreshToken: string;
+}
---
+++ src/app/models/post.model.ts
@@ -0,0 +1,20 @@
+//define the Post interface
+export interface Post {
+  id: number;
+  title: string;
+  body: string;
+  tags: string[];
+  reactions: {
+    likes: number;
+    dislikes: number;
+  };
+  views: number;
+  userId: number;
+}
+
+export interface PostsResponse {
+  posts: Post[];
+  total: number;
+  skip: number;
+  limit: number;
+}
