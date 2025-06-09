a29f49013680e3d5ea832b8afa20dac7e88bd2d5
remant <paudelremant45@gmail.com>
2025-06-09 23:42:24 +0545

    feat: implement NotificationService and update notifications component for message handling

---
+++ src/app/core/services/notification/notification.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NotificationService {
+  private _notificationCount = new BehaviorSubject<number>(0);
+  notificationCount$ = this._notificationCount.asObservable();
+
+  private _messages = new BehaviorSubject<string[]>([]);
+  messages$ = this._messages.asObservable();
+
+  updateMessages(messages: string[]) {
+    this._messages.next(messages);
+    this._notificationCount.next(messages.length);
+  }
+}
--- src/app/pages/notifications/notifications.component.ts
+++ src/app/pages/notifications/notifications.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-notifications',
   standalone: false,
   templateUrl: './notifications.component.html',
-  styleUrl: './notifications.component.scss'
+  styleUrl: './notifications.component.scss',
 })
 export class NotificationsComponent {
+  messages: string[] = [];
 
+  constructor(private router: Router) {
+    const nav = this.router.getCurrentNavigation();
+    if (nav?.extras?.state?.['messages']) {
+      this.messages = nav.extras.state['messages'];
+    }
+  }
 }
