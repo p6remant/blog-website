295349850adfef5cb68e35526eb5c611d05cfb54
remant <paudelremant45@gmail.com>
2025-06-09 23:43:05 +0545

    feat: integrate ToastService for login success and error notifications

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { LoginService } from '../../core/services/login/login.service';
 import { Router } from '@angular/router';
+import { ToastService } from '../../core/services/toast/toast.service';
 
 @Component({
   selector: 'app-login',
@@ -15,7 +16,8 @@ export class LoginComponent {
   constructor(
     private fb: FormBuilder,
     private loginService: LoginService,
-    private router: Router
+    private router: Router,
+    private toast: ToastService
   ) {
     this.loginForm = this.fb.group({
       username: ['', [Validators.required, Validators.minLength(5)]],
@@ -32,16 +34,20 @@ export class LoginComponent {
 
       this.loginService.login(payload).subscribe({
         next: (res) => {
-          console.log('Login successful:', res);
+          // console.log('Login successful:', res);
           this.loginService.setAccessToken(res.accessToken);
           this.loginService.setRefreshToken(res.refreshToken);
           this.loginService.setUsername(res.username);
           this.loginService.setUserId(res.id);
+          this.toast.success('Login successful');
           this.router.navigate(['/']);
         },
         error: (err) => {
-          console.error('Login error:', err);
-          alert(err.error.message);
+          //console.error('Login error:', err);
+          //alert(err.error.message);
+          this.toast.error(
+            err.error.message || 'Something went wrong. Please try later!'
+          );
         },
       });
     }
